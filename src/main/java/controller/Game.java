93600d6b86f8976cbb6f5e514182b7c65c5dd1a4
Roland Valdez <valdez.roland.23@gmail.com>
2021-08-02 14:30:36 -0500

    working on about page

---
+++ src/main/java/controller/Game.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Game {
+    @GetMapping("/")
+    public String landing(){
+        return "index";
+    }
+    @GetMapping("/rules")
+    public String rules(){
+        return "rules";
+    }
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+    @GetMapping("/game")
+    public String game(){
+        return "game";
+    }
+}
